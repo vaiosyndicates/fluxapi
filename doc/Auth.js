fb6ccd5c4f410ed6648734572a6e0c8a74b3ceeb
kresna <kresnafti2013@gmail.com>
2023-08-25 23:00:12 +0700

    update swagger doc for response login

--- doc/Auth.js
+++ doc/Auth.js
@@ -130,13 +130,11 @@
  *                  type: string
  *                  example: success
  *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       token:
- *                         type: string
- *                         description: The user ID.
- *                         example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+ *                   type: object
+ *                   properties:
+ *                    token:
+ *                     type: string
+ *                     description: The user ID.
+ *                     example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
  * 
  */
