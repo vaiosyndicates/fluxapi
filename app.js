1f7510c3c9e542d7b0f8aa9ebf70734cec040583
kresna <kresnafti2013@gmail.com>
2023-09-02 00:37:51 +0700

    minor change for port

--- app.js
+++ app.js
@@ -69,8 +69,8 @@ app.use((req, res, next) => {
 })
 
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
  
 module.exports = app;
