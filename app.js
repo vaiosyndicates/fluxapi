d9fb083c084f4cb9f0476a476b7859c7ae32e48f
kresna <kresnafti2013@gmail.com>
2023-09-02 00:48:02 +0700

    change port

--- app.js
+++ app.js
@@ -69,8 +69,8 @@ app.use((req, res, next) => {
 })
 
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
  
 module.exports = app;
